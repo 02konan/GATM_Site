42d90fa9bd8e1f41687f41f5f670a724bbd67bfb
02konan <152191746+02konan@users.noreply.github.com>
2025-08-20 01:09:14 +0000

    public a la racine

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -74,7 +74,7 @@ const About: React.FC = () => {
             {/* Logo avec texte */}
             <div className="flex items-center space-x-3 mb-6">
               <div className="w-20 h-20 rounded-xl flex items-center justify-center">
-                <img src="GATM_Site\src\public\img\logo.jpg" alt="GATM Logo" className="w-full h-full object-contain" />
+                <img src="\img\logo.jpg" alt="GATM Logo" className="w-full h-full object-contain" />
               </div>
               <div className="text-navy font-semibold">
                 <div className="text-sm">Groupe d'Alternance</div>
